5c6936c0662d0b679107b016465e5755beff557f
ggthkk <babagatom1@gmail.com>
2023-08-28 15:47:28 +0700

    update

--- plugins/aos.js
+++ plugins/aos.js
@@ -3,7 +3,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
-    if (typeof window !== "undefined") {
-        nuxtApp.AOS = AOS.init({ disable: "mobile" });
-    }
+  if (typeof window !== "undefined") {
+    nuxtApp.AOS = AOS.init({ disable: "mobile" });
+  }
 });
